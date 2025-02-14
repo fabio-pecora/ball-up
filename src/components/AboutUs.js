ad57ee1b2087b7dca377bfc262d37d0313f09dad
fabio-pecora <fabiopecora01@gmail.com>
2025-02-14 14:31:14 -0500

    about page almost finished

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../AboutUs.css';
-
-
+import fabioPic from '../assets/fabio.PNG';
 
 const AboutUs = () => {
   return (
@@ -12,7 +11,7 @@ const AboutUs = () => {
         </h2>
         <ul>
           <li>
-            <span role="img" aria-label="map">📍</span> Find Courts Near You
+            <span role="img" aria-label="map">📍</span> Find Courts Near You 
           </li>
           <li>
             <span role="img" aria-label="basketball">🏀</span> Show Your Game
@@ -25,33 +24,54 @@ const AboutUs = () => {
           </li>
         </ul>
       </section>
-
-
-
-
-      
       <section className="section operate">
         <h2>Where We Operate</h2>
         <p>We are currently active in the 5 boroughs of New York City!(Staten Island, Brooklyn, Manhattan, Bronx, and Queens).</p>
         <i className="icon fas fa-globe-americas"></i>
       </section>
-      
       <section className="section future-goal">
         <h2>Our Future Goal</h2>
-        <p>We aim to expand globally, introducing innovative ways to enhance basketball culture through technology and community-driven initiatives.</p>
+        <ul>
+          <li>
+            <span role="img" aria-label="map">📱</span> Lounch a mobile app
+          </li>
+          <li>
+            <span role="img" aria-label="basketball">🌆</span> Expand to other cities
+          </li>
+          <li>
+            <span role="img" aria-label="chat">🏀</span> Host tournaments
+          </li>
+          <li>
+            <span role="img" aria-label="network">🏆</span> Maintain leaderboards
+          </li>
+        </ul>
         <i className="icon fas fa-rocket"></i>
       </section>
-      
-      <section className="section team">
+      <section className="section meet-team">
         <h2>Meet the Team</h2>
-        <p>Our team is made up of passionate basketball enthusiasts, developers, and community leaders who strive to make a difference in the game.</p>
-        <i className="icon fas fa-users"></i>
-      </section>
-      
-      <section className="section involvement">
-        <h2>How to Get Involved</h2>
-        <p>Want to be part of the BALL UP movement? Join us today and contribute to the growing basketball community.</p>
-        <i className="icon fas fa-hands-helping"></i>
+        <div className="team-member">
+          <img
+            src={fabioPic}
+            alt="Fabio Pecora"
+            className="team-photo"
+          />
+          <div className="team-info">
+            <h3>Fabio Pecora</h3>
+            <p>
+              I have loved and played basketball since I was 4.
+              Since I moved to NY I started dreaming of a basketball social network where people who share
+              the same passion can come together to find courts, meet people, and love the game.
+            </p>
+            <div className="contact-info">
+              <p>
+                <span role="img" aria-label="email">📧</span> fabiopecora01@gmail.com
+              </p>
+              <p>
+                <span role="img" aria-label="mobile">📱</span> +1 9295295942
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
